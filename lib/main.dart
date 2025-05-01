90b9ce5e95b36ee169eeffab4b4caa46c6a219ea
sena <sena201305@gmail.com>
2025-05-01 17:15:30 +0900

    Update color scheme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter(){
+    setState((){
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -112,10 +118,21 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment:MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+          onPressed: _decrementCounter,
+          tooltip: 'Decrement',
+          child: const Icon(Icons.remove),
+          ),
+        ]
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
